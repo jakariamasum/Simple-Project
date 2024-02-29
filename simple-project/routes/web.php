2dee98691c13979fbd2c0e3761525ff96bee3273
jakariamasum <jmphy698@gmail.com>
2024-02-29 23:47:33 +0600

    hbb

--- simple-project/routes/web.php
+++ simple-project/routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('categories',[App\Http\Controllers\CategoryController::class,'index']);
+
+  
 Route::get('/', function () {
     return view('welcome');
 });
