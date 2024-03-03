15e1bfbf5f8a5acd057b60c79d1f8b07e673e0db
jakariamasum <jmphy698@gmail.com>
2024-03-04 01:08:28 +0600

    component create and rendered

---
+++ simple-project/resources/views/components/alert-success.blade.php
@@ -0,0 +1,7 @@
+@props(['message'])
+<div class="alert alert-success">
+<h1>{{$message}}</h1>
+</div>
+<div class="alert alert-info">
+    <h1>Static here</h1>
+</div>
--- simple-project/resources/views/frontend/index.blade.php
+++ simple-project/resources/views/frontend/index.blade.php
@@ -1,4 +1,21 @@
 <x-my-layout>
+    <x-slot:title>
+        My title
+    </x-slot:title>
+    <div class="py-3">
+        <div class="container">
     <h1>Hello guys</h1>
+    @php 
+        $NewMessage="hello";
+    @endphp
+    <x-alertSuccess :message="$NewMessage"/>
+    
+        </div>
+    </div>
+    <x-slot:scripts>
+        <script>
+            alert('Hello')
+        </script>
+    </x-slot:scripts>
 
 </x-my-layout>
